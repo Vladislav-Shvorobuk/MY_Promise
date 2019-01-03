69fff4c1085d7c3bec2e0a6335cd1db54fd16db8
Vladislav <vladislav.shvorobuk@gmail.com>
2019-01-03 12:31:30 +0200

    create constructor

--- src/promise.js
+++ src/promise.js
@@ -1,5 +1,35 @@
 class OwnPromise {
+  constructor(executor) {
+    this.state = 'PENDING';
+    this.value = null;
+    this.queue = [];
+    executor(OwnPromise.resolve, OwnPromise.reject);
+  }
 
+  // static all() {
+
+  // }
+  // static race() {
+
+  // }
+  // static resolve() {
+
+  // }
+  // static reject() {
+
+  // }
+
+
+  // then() {
+
+  // }
+  // catch() {
+  // }
+  // finally() {
+  // }
+  // get [Symbol.toStringTag]() {
+  //   return this.name;
+  // }
 }
 
 module.exports = OwnPromise;

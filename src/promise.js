eb9d58f8bf9c52daff639fc564200fb4e975f60f
Vladislav <vladislav.shvorobuk@gmail.com>
2019-01-04 13:39:08 +0200

    add resolve

--- src/promise.js
+++ src/promise.js
@@ -1,20 +1,33 @@
 class OwnPromise {
   constructor(executor) {
+    if (typeof executor !== 'function') {
+      throw new TypeError('Executor is not a function');
+    }
+
     this.state = 'PENDING';
     this.value = null;
     this.queue = [];
     executor(OwnPromise.resolve, OwnPromise.reject);
   }
 
+  static resolve(value) {
+    if (this.state !== 'PENDING') {
+      return undefined;
+    }
+    return value && ({}).hasOwnProperty.call(value, 'then') ? value
+      : new OwnPromise(resolve => {
+        resolve(value);
+      });
+  }
   // static all() {
 
   // }
   // static race() {
 
   // }
-  // static resolve() {
-
-  // }
+//   static resolve(value) {
+//     return new OwnPromise(resolve => resolve(value));
+//   }
   // static reject() {
 
   // }

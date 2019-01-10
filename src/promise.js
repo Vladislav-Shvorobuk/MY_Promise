c73669c321054adf0c7adc99929340a6b6721bb3
Vladislav <vladislav.shvorobuk@gmail.com>
2019-01-10 11:37:14 +0200

    Timo_lesson

--- src/promise.js
+++ src/promise.js
@@ -1,5 +1,59 @@
-class OwnPromise {
+/*eslint-disable*/
+const RESOLVED = 'RESOLVED';
+const PENDING = 'PENDING';
+const REJECTED = 'REJECTED';
 
+let cb = null;
+
+class OwnPromise {
+  constructor(exeq) {
+    this.state = PENDING;
+    this.callbacks = [];
+
+   const resolve = (data) => {
+      if(this.state !== PENDING){
+        return;
+      }
+      this.state === RESOLVED;
+      this.value = data;
+      this.callbacks.forEach(({res, rej}) => {
+        //нужна проверка
+        this.value = res(this.value);
+      });
+   }
+   const reject = (error) => {
+
+   }
+
+   try {
+    exeq(resolve, reject);
+   } catch (e){
+     reject(e);
+   }
+
+
+
+  }
+
+  // вынести логику 
+
+  then(res, rej) {
+    // cb = res;
+    if(this.state == RESOLVED){
+      res(this.value);
+    }
+    this.callbacks.push({res, rej});
+    return this; //новый инстенс своего промиса
+  }
+  catch(rej) {
+
+  }
 }
 
 module.exports = OwnPromise;
+
+new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    resolve(1);
+  });
+});

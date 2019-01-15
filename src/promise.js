242f8a06688349ef1bfcc80e2cf1d7e42330da19
Vladislav <vladislav.shvorobuk@gmail.com>
2019-01-15 11:36:06 +0200

    fixed linter tests

--- src/promise.js
+++ src/promise.js
@@ -1,20 +1,18 @@
-/*eslint-disable*/
-const PENDING = "PENDING";
-const FULFILLED = "FULFILLED";
-const REJECTED = "REJECTED";
+const PENDING = 'PENDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
 
 class OwnPromise {
   constructor(executor) {
-
     this.onFulfilledCallbacks = [];
     this.onRejectedCallbacks = [];
     this.status = PENDING;
-    
+
     if (typeof executor !== 'function') {
       throw new TypeError('Not a function');
     }
 
-const resolve = res => {
+    const resolve = res => {
       if (res instanceof OwnPromise) {
         res.then(resolve);
       }
@@ -42,7 +40,6 @@ const resolve = res => {
       }
     };
 
-
     try {
       executor(resolve, reject);
     } catch (error) {
@@ -50,14 +47,12 @@ const resolve = res => {
     }
   }
 
-// THEN
+  // THEN
   then(onFulfilled, onRejected) {
-    // if (!(this instanceof OwnPromise)) {
-    //   throw new TypeError('Not a function');
-    // }
-
-    onFulfilled = typeof onFulfilled === "function" ? onFulfilled : value => value;
-    onRejected =  typeof onRejected === "function" ? onRejected : error => { error; };
+    const onFulf = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    const onRej = typeof onRejected === 'function' ? onRejected : error => {
+      error;
+    };
 
     return new this.constructor((resolve, reject) => {
       if (typeof resolve !== 'function' || typeof reject !== 'function') {
@@ -66,151 +61,103 @@ const resolve = res => {
 
       if (this.status === FULFILLED) {
         setTimeout(() => {
-          const resCallback = onFulfilled(this.value);
-          OwnPromise.checkFunction(resCallback, resolve, reject);
+          const result = onFulf(this.value);
+          OwnPromise.checkFunction(result, resolve, reject);
         }, 0);
       } else if (this.status === REJECTED) {
         setTimeout(() => {
-          const resCallback = onRejected(this.error);
-          OwnPromise.checkFunction(resCallback, resolve, reject);
+          const reason = onRej(this.error);
+          OwnPromise.checkFunction(reason, resolve, reject);
         }, 0);
       } else if (this.status === PENDING) {
         this.onFulfilledCallbacks.push(value => {
           try {
-            const resCallback = onFulfilled(value);
-            OwnPromise.checkFunction(resCallback, resolve, reject);
+            const result = onFulf(value);
+            OwnPromise.checkFunction(result, resolve, reject);
           } catch (error) {
             reject(error);
           }
         });
 
         this.onRejectedCallbacks.push(error => {
-          // try {
-            const resCallback = onRejected(error);
-            OwnPromise.checkFunction(resCallback, resolve, reject);
-          // } catch (error) {
-          //   reject(error);
-          // }
+          const reason = onRej(error);
+          OwnPromise.checkFunction(reason, resolve, reject);
         });
       }
     });
   }
 
-// CHECK_FUNCTION
+  // CHECK_FUNCTION
   static checkFunction(result, resolve, reject) {
     if (typeof resolve !== 'function' || typeof reject !== 'function') {
       throw new TypeError('Not a function');
     }
 
     try {
-      if (result instanceof OwnPromise){
+      if (result instanceof OwnPromise) {
         result.then(a => resolve(a));
-      }else {
+      } else {
         resolve(result);
       }
-    } catch (e){
+    } catch (e) {
       reject(e);
     }
 
-    if (result instanceof OwnPromise){
+    if (result instanceof OwnPromise) {
       result.then(a => reject(a));
-    }else {
+    } else {
       reject(result);
     }
-
-
-    // let called = false;
-
-    // if (result instanceof OwnPromise) {
-    //   try {
-    //     let then = result.then;
-    //     if (typeof then === "function") {
-    //       then.call(
-    //         x,
-    //         y => {
-    //           if (called) return;
-    //           called = true;
-    //           checkFunction(y, resolve, reject);
-    //         },
-    //         error => {
-    //           if (called) return;
-    //           called = true;
-    //           reject(error);
-    //         }
-    //       );
-    //     } else {
-    //       resolve(result);
-    //     }
-    //   } catch (e) {
-    //     if (called) return;
-    //     called = true;
-    //     reject(e);
-    //   }
-    // } else {
-    //   resolve(result);
-    // }
   }
 
   // CATCH
   catch(onRejected) {
     return this.then(null, onRejected);
   }
-
+  // FINALLY
   finally(fn) {
     return this.then(res => OwnPromise.resolve(fn()).then(() => res), err => OwnPromise.reject(fn()).then(() => err));
   }
 
   // ALL
   static all(promises) {
-    if(typeof this !== 'function'){
-      throw TypeError('this !== function');
-    }
-
-    if(promises.length == undefined ){
-      return new OwnPromise((resolve, reject) => {
-        reject(new TypeError('Not an itterable'));
-      });
-    }
-
-    if(promises.length == 0 ){
-      return new OwnPromise((resolve, reject) => {
+    if (promises.length === 0) {
+      return new this((resolve, reject) => {
         resolve([]);
       });
     }
 
-
-
     return new this((resolve, reject) => {
-      let result = [];
+      const results = [];
       let count = 0;
-      for (let i = 0; i < promises.length; i++) {
-        promises[i].then(
-          data => {
-            result[i] = data;
-            if (++count === promises.length) {
-              resolve(result);
-            }
-          },
-          err => {
-            reject(err);
+
+      promises.forEach((promise, i) => {
+        promise.then(res => {
+          results[i] = res;
+          count += 1;
+
+          if (count === promises.length) {
+            resolve(results);
           }
-        );
-      }
+        }, err => {
+          reject(err);
+        });
+      });
     });
   }
 
-   // RACE
-   static race(promises) {
+  // RACE
+  static race(promises) {
     if (typeof this !== 'function') {
       throw new TypeError('this is not a constructor');
     }
 
-    if(promises == [] ){
+    if (promises === []) {
       return new OwnPromise((resolve, reject) => {
       });
     }
-    
-    if(promises.length == undefined ){
+
+    if (promises.length === undefined) {
       return new OwnPromise((resolve, reject) => {
         reject(new TypeError('Not an itterable'));
       });
@@ -230,11 +177,12 @@ const resolve = res => {
     });
   }
 
+  // RESOLVE
   static resolve(value) {
     if (value instanceof OwnPromise) {
       return value;
     }
-    return new this(function(resolve, reject) {
+    return new this((resolve, reject) => {
       if (typeof resolve !== 'function' || typeof reject !== 'function') {
         throw new TypeError('Not a function');
       }
@@ -242,6 +190,7 @@ const resolve = res => {
     });
   }
 
+  // REJECT
   static reject(error) {
     return new this((resolve, reject) => {
       if (typeof resolve !== 'function' || typeof reject !== 'function') {
@@ -255,45 +204,37 @@ const resolve = res => {
 module.exports = OwnPromise;
 
 
+// const promise = new OwnPromise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(100);
+//   }, 2000);
+//   // resolve(100);
+// });
 
+// console.log(promise);
 
+// promise
+//   .then(function(value) {
+//     console.log(`then =>${value}`);
+//   }).then(() => 200);
 
-
-
-
-const promise = new OwnPromise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(100);
-  }, 2000);
-  // resolve(100);
-});
-
-console.log(promise);
-
-promise
-  .then(function(value) {
-    console.log("then =>" + value);
-  }).then(() => {
-    return 200;
-  })
-
-  promise.then(function(value) {
-    console.log("then =>" + value);
-    return 300;
-  })
-  .then(function(value) {
-    console.log("then =>" + value);
-    throw "Я иду в catch";
-  })
-  .catch(function(value) {
-    console.log("cath =>" + value);
-    return 500;
-  })
-  .then(function(value) {
-    console.log("then =>" + value);
-    return 600;
-  })
-  .then(function(value) {
-    console.log("then =>" + value);
-    return 700;
-  });
+// promise.then(function(value) {
+//   console.log(`then =>${value}`);
+//   return 300;
+// })
+//   .then(function(value) {
+//     console.log(`then =>${value}`);
+//     throw 'Я иду в catch';
+//   })
+//   .catch(function(value) {
+//     console.log(`cath =>${value}`);
+//     return 500;
+//   })
+//   .then(function(value) {
+//     console.log(`then =>${value}`);
+//     return 600;
+//   })
+//   .then(function(value) {
+//     console.log(`then =>${value}`);
+//     return 700;
+//   });
